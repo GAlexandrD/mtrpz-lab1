b21f48cabcb693763574d67868f3383027c6ced9
suxarik <suxarik312004gor@gmail.com>
2023-03-04 16:00:22 +0200

    removed code duplication

--- interactive.js
+++ interactive.js
@@ -4,31 +4,16 @@ const rl = createInterface(process.stdin, process.stdout);
 
 const ask = async (question) => rl.question(question);
 
-const inputA = async () => {
-  const answer = await ask('input a: ');
+const input = async (question, isZeroAllowed) => {
+  const answer = await ask(question);
   const isValid = isValidNumber(+answer);
-  const isZero = +answer === 0;
-  if (isValid && !isZero) return +answer;
-  if (!isValid) {
-    rl.write(`Error. Expected a valid real number, got ${answer} instead\n`);
-  } else rl.write(`a cannot be 0\n`);
-  return await inputA();
-};
-
-const inputB = async () => {
-  const answer = await ask('input b: ');
-  const isValid = isValidNumber(+answer);
+  if (!isZeroAllowed && +answer === 0) {
+    rl.write(`this param cannot be 0\n`);
+    return await input(question, isZeroAllowed);
+  }
   if (isValid) return +answer;
   rl.write(`Error. Expected a valid real number, got ${answer} instead\n`);
-  return await inputB();
-};
-
-const inputC = async () => {
-  const answer = await ask('input c: ');
-  const isValid = isValidNumber(+answer);
-  if (isValid) return +answer;
-  rl.write(`Error. Expected a valid real number, got ${answer} instead\n`);
-  return await inputC();
+  return await input(question, isZeroAllowed);
 };
 
 function isValidNumber(num) {
@@ -36,8 +21,8 @@ function isValidNumber(num) {
 }
 
 export const askValues = async () => {
-  const a = await inputA();
-  const b = await inputB();
-  const c = await inputC();
+  const a = await input('input a:', false);
+  const b = await input('input b:', true);
+  const c = await input('input c:', true);
   return { a, b, c };
 };
